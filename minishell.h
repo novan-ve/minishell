69fc3749f2346c857e3e79164333096cf53a2a75
novan-ve <novan-ve@student.codam.nl>
2020-05-10 12:39:26 +0200

    Added vector functions to header

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: novan-ve <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/03/09 13:46:12 by novan-ve      #+#    #+#                 */
-/*   Updated: 2020/05/09 13:37:43 by novan-ve      ########   odam.nl         */
+/*   Updated: 2020/05/10 12:33:17 by novan-ve      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # define EXIT_SUCCES 0
 # define EXIT_FAILURE 1
+
+# define VECTOR_INIT_CAPACITY 4
 
 # include <unistd.h>
 # include <stdlib.h>
@@ -32,6 +34,9 @@ typedef	struct	s_minishell
 	char		*line;
 	char		**args;
 	int			status;
+	void		**env;
+	int			capacity;
+	int			total;
 }				t_minishell;
 
 void			ft_read_line(t_minishell *sh);
@@ -50,5 +55,14 @@ size_t			ft_strlen(const char *s);
 void			ft_putstr(char *s);
 void			ft_putendl(char *s);
 int				ft_strcmp(const char *s1, const char *s2);
+
+void			ft_vector_init(t_minishell *sh);
+int				ft_vector_total(t_minishell *sh);
+static void		ft_vector_resize(t_minishell *sh, int x);
+void			ft_vector_add(t_minishell *sh, void *s);
+void			ft_vector_set(t_minishell *sh, int index, void *s);
+void			*ft_vector_get(t_minishell *sh, int index);
+void			ft_vector_delete(t_minishell *sh, int index);
+void			ft_vector_free(t_minishell *sh);
 
 #endif
