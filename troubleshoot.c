dfedff3cd49741e56fe1fde3c83410a887e31713
Telly <abobas@student.codam.nl>
2020-05-13 04:41:11 +0200

    cleaned

--- troubleshoot.c
+++ troubleshoot.c
@@ -6,7 +6,7 @@
 /*   By: abobas <abobas@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/12 13:41:10 by abobas        #+#    #+#                 */
-/*   Updated: 2020/05/13 02:17:59 by abobas        ########   odam.nl         */
+/*   Updated: 2020/05/13 04:39:46 by abobas        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,5 @@ void        show_args(t_minishell *sh)
     {
         printf("%s\n", sh->args[i]);
         i++;
-    }
-}
-
-void        show_env(t_minishell *sh)
-{
-    t_env *list;
-
-	list = sh->first_element;
-	while (list)
-    {
-        printf("%s\n", list->data);
-		list = list->next;
     }
 }
