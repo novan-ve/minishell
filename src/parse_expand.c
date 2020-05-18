0a07a7c84e249230e7b21a2e46c048e9171b5ea4
Telly <abobas@student.codam.nl>
2020-05-18 17:16:51 +0200

    v2

---
+++ src/parse_expand.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   parse_expand.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: abobas <abobas@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/05/17 23:45:04 by abobas        #+#    #+#                 */
+/*   Updated: 2020/05/18 00:20:32 by abobas        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/minishell.h"
+
+int		parse_expand(t_minishell *sh)
+{
+	int		i;
+	int		j;
+	
+	i = 0;
+	while (i < sh->line_count)
+	{
+		j = 0;
+		while (j < sh->arg_count[i])
+		{
+			//// IF VARIABLES IN ARG >> EXPAND VARIABLES
+			//// IF ERROR >> RETURN (0)
+			j++;
+		}
+		i++;
+	}
+	if (!parse_sanitize(sh)) ///// HAALT ALLE ARGUMENTS NULL-POINTER ER TUSSEN UIT
+		return (0);
+	return (1);
+}
