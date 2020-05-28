49090e94ea23ed3e32c06dd1510c571a2648895f
Telly <abobas@student.codam.nl>
2020-05-28 21:27:24 +0200

    test

--- src/execute.c
+++ src/execute.c
@@ -6,7 +6,7 @@
 /*   By: abobas <abobas@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/19 00:25:13 by abobas        #+#    #+#                 */
-/*   Updated: 2020/05/28 21:11:47 by abobas        ########   odam.nl         */
+/*   Updated: 2020/05/28 21:26:18 by abobas        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ void	execute(char **av, t_minishell *sh)
 {
 	pid_t	pid;
 
-	av[0] = get_executable(av[0], sh);
+	//av[0] = get_executable(av[0], sh);
 	if (!av[0])
 		return ;
 	pid = fork();
@@ -73,7 +73,6 @@ void	execute(char **av, t_minishell *sh)
 	{
 		if (execve(av[0], av, sh->env->data) < 0)
 		{
-			free(av[0]);
 			put_error("Command not found");
 			exit(127);
 		}
