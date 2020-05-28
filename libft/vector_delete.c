8c5a76e9135520020c62a008d7dad22049f421f4
Telly <abobas@student.codam.nl>
2020-05-28 20:46:57 +0200

    changes to vector_delete

--- libft/vector_delete.c
+++ libft/vector_delete.c
@@ -6,7 +6,7 @@
 /*   By: abobas <abobas@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/16 00:45:23 by abobas        #+#    #+#                 */
-/*   Updated: 2020/05/22 13:36:05 by abobas        ########   odam.nl         */
+/*   Updated: 2020/05/28 20:44:57 by abobas        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@ void	vector_delete(t_vector *v, int index)
 {
 	if (index < 0 || index >= v->total)
 		return ;
-	if (v->data[index])
-		free(v->data[index]);
+	free(v->data[index]);
 	while (index < v->total - 1)
 	{
 		v->data[index] = v->data[index + 1];
