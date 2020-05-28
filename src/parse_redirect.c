fc8ab82d4397438b7a7840f3fd6a893d89033e40
Telly <abobas@student.codam.nl>
2020-05-28 20:15:38 +0200

    fix to > redirection, truncating file

--- src/parse_redirect.c
+++ src/parse_redirect.c
@@ -6,7 +6,7 @@
 /*   By: abobas <abobas@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/23 15:51:50 by abobas        #+#    #+#                 */
-/*   Updated: 2020/05/26 22:01:55 by abobas        ########   odam.nl         */
+/*   Updated: 2020/05/28 20:14:28 by abobas        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int		parse_output_redirect(t_minishell *sh, int i, int y)
 	if (!ft_strcmp(sh->args[i][y], ">>"))
 		fd = open(sh->args[i][y + 1], (O_CREAT | O_WRONLY | O_APPEND), 0666);
 	else
-		fd = open(sh->args[i][y + 1], (O_CREAT | O_WRONLY), 0666);
+		fd = open(sh->args[i][y + 1], (O_CREAT | O_WRONLY | O_TRUNC), 0666);
 	if (fd < 0)
 	{
 		put_error(strerror(errno));
