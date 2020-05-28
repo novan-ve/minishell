a324a5694b04b384693d769cdd13210244fb634d
Telly <abobas@student.codam.nl>
2020-05-28 23:21:01 +0200

    test

--- src/path.c
+++ src/path.c
@@ -6,7 +6,7 @@
 /*   By: abobas <abobas@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/22 13:10:17 by abobas        #+#    #+#                 */
-/*   Updated: 2020/05/28 23:18:36 by abobas        ########   odam.nl         */
+/*   Updated: 2020/05/28 23:20:45 by abobas        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,9 +109,9 @@ char			*get_executable(char *arg, t_minishell *sh)
 	path = get_env(sh, "PATH");
 	if (!path)
 		return (arg);
-	return (arg);
 	path_array = ft_split(path, ':');
 	free(path);
+	return (arg);
 	if (!path_array)
 	{
 		put_error(strerror(errno));
